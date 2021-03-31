e3771e9080572b12e0aa646ceeced3a17d6def02
Harry Bett <harrybett89@gmail.com>
2021-03-31 15:46:31 +0300

    add publishingDate

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
   showDetails:boolean;
-  constructor(public id: number, public quote: string, public author: string){
+  constructor(public id: number, public quote: string, public author: string, public publishingDate: Date){
     this.showDetails=false;
   }
 }
