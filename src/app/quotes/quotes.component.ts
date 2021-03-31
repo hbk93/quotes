479f8462427156ffc1d37b51003b8df7385b1094
Harry Bett <harrybett89@gmail.com>
2021-03-31 12:44:28 +0300

    add quotes array

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
+
 
 @Component({
   selector: 'app-quotes',
@@ -6,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  
+  quotes:Quotes[] = [
+    {id:1, quote:'time flies', author:'wagwan'},
+    {id:2, quote:'as it should', author:'mzito'}
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
