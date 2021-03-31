c03d97695d76438f26558c86cafbc06fed99bb8a
Harry Bett <harrybett89@gmail.com>
2021-03-31 17:07:26 +0300

    create timePassed pipe

--- src/app/time-passed.pipe.ts
+++ src/app/time-passed.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimePassedPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayWithoutTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(value-todayWithoutTime)
+    const secondsInDay = 86400;
+    const minutesInHour = 60;
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+
+    if (dateCounter >= 1 && value < todayWithoutTime){
+      return dateCounter;
+    }    
+    else {
+      return 0;
+    }
   }
 
 }
